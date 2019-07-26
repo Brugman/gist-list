1fb97164b5067919178f48f88e5419a61eb7fa7f
Tim Brugman <tim@timbrugman.com>
2019-07-26 13:46:38 +0200

    Updated favicons.

--- app/core-head.php
+++ app/core-head.php
@@ -12,8 +12,10 @@
     <title>Gist List</title>
 
     <!-- favicon -->
-    <link rel="apple-touch-icon" href="/assets/images/favicon.png">
-    <meta name="msapplication-TileImage" content="/assets/images/favicon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 
     <!-- link css -->
     <link rel="stylesheet" href="/assets/vendor/datatables-1.10.19/css/jquery.dataTables.min.css">
